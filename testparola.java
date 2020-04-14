baa5c3672a2d5623799100f53f6129b3018c023c
BittiBF <63063056+BittiBF@users.noreply.github.com>
2020-04-14 11:11:24 +0200

    Update testparola.java

--- testparola.java
+++ testparola.java
@@ -1,70 +1,90 @@
 package it.uninsubria.dista.dictionary;
 
-import java.awt.Graphics;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.EventQueue;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
-
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JTextArea;
 import javax.swing.JList;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.SystemColor;
-import javax.swing.BoxLayout;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
 import java.awt.GridLayout;
+import java.awt.Color;
 
 public class testparola extends JFrame {
 
 	private JPanel contentPane;
 	ArrayList<String> elencoVerifica = new ArrayList<>();
+	ArrayList<String> giuste = new ArrayList<>();
+	ArrayList<String> sbagliate = new ArrayList<>();
 	List<Definition> definitionLista;
 	DefaultListModel<Term> model = new DefaultListModel<>();
+	JLabel daticorretti = new JLabel();
+	JLabel datiscorretti = new JLabel();
+	
+	
 	
 	JPanel panel = new JPanel();
 	int cont = 1;
+	int intg = 0;
+	int ints = 0;
+	
 	public testparola(ArrayList<String> cs) {
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 579, 351);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 664, 364);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		
-		panel.setBounds(10, 29, 641, 215);
+		panel.setBounds(10, 29, 628, 218);
 		contentPane.add(panel);
 		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JList<Term> list = new JList<>( model );
-		list.setBackground(SystemColor.menu);
-		panel.add(list);
 		
 		JLabel lblNewLabel = new JLabel("PLAYER 1");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblNewLabel.setBounds(223, 0, 107, 29);
 		contentPane.add(lblNewLabel);
-
-		JScrollPane sc = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		panel.add(sc);
+	
+		
+		JList<Term> list = new JList<>( model );
+		list.setBackground(SystemColor.menu);
+		panel.add(list);
+		
+				JScrollPane sc = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+				panel.add(sc);
+				JLabel titolocorrette = new JLabel("PAROLE CORRETTE");
+				titolocorrette.setForeground(Color.GREEN);
+				titolocorrette.setBounds(10, 258, 126, 14);
+				contentPane.add(titolocorrette);
+				
+				
+				daticorretti.setBounds(146, 258, 46, 14);
+				contentPane.add(daticorretti);
+				
+				JLabel titolosbagliate = new JLabel("PAROLE SBAGLIATE");
+				titolosbagliate.setForeground(Color.RED);
+				titolosbagliate.setBounds(251, 258, 131, 14);
+				contentPane.add(titolosbagliate);
+				
+				
+				datiscorretti.setBounds(392, 258, 46, 14);
+				contentPane.add(datiscorretti);
+	
 		
 		int size = cs.size();
 		String testo;
@@ -99,7 +119,7 @@ public class testparola extends JFrame {
 
 	public void checkparola(String parola) throws ZipException, IOException, InvalidKey {
 		cercaParola cp = new cercaParola();
-		Loader load=new Loader();
+		 Loader load=new Loader();
 		String file_dizionario= "dict-it.oxt";
 		File dizionario=new File(file_dizionario);
         Dictionary d=load.loadDictionaryFromFile(dizionario);
@@ -118,23 +138,38 @@ public class testparola extends JFrame {
 			if (cp.ControlloFinale(termine)) {
 				// termine.toUpperCase()
 				s = String.valueOf(cont);
+				giuste.add(termine);
 				Term nt1 = new Term(s + " - " + termine.toUpperCase() + "\n");
 				model.addElement(nt1);
 				model.addElement(d.getTerm(termine));
                  cont++;
+                 intg ++;
                   }
     
 			else {
 				s = String.valueOf(cont);
-				Term nt = new Term (s + " - " + termine + "\n");
+				sbagliate.add(termine);
+				Term nt = new Term (s + " - " + termine.toUpperCase() + "\n");
 				Term nt1 = new Term ("Parola non trovata");
 				model.addElement(nt);
 				model.addElement(nt1);
 				cont++;
+				ints++;
 			}
+		    
+			String stringg = String.valueOf(intg);
+			daticorretti.setText(": " + stringg);
+			String strings = String.valueOf(ints);
+			datiscorretti.setText(": " + strings);
+			
 			
 	}
-	}
+   }
+	
+
+		 
+	   
+	
 	
 	
 	
