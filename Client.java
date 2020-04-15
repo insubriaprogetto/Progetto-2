ad35e85526d16a2b88f09cf33d9164aca61151ac
DaddCagnez <43757085+DaddCagnez@users.noreply.github.com>
2020-04-15 18:23:02 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -7,23 +7,47 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class Client {
+import bitti.TabellaGioco;
+
+public class Client extends TabellaGioco {
 	
+	//String c = new String("prova");
+	String c;
 	
 	ClientConnection cc;
 	
 	public static void main(String[] args) {
 		new Client();
-
+		
 	}
 	
 public Client(){
+
+//al posto di questo ci saraà la registrazione/login clinet	
+	Scanner nome = new Scanner(System.in);
+	System.out.println("inserisci il tuo nome:");
+	c = nome.nextLine();
+	System.out.println("il nome "+c+" è stato inserito" );
+//---------------------------------------------------------
+	
+	
+	
+	
 	
 	try {
 		Socket s = new Socket("localhost", 3333);
 		 cc = new ClientConnection(s,this);
 		 cc.start();
-		 	
+		 
+		 
+		 TabellaGioco p = new TabellaGioco();
+		 p.setVisible(true);
+			
+		 
+		 
+		 
+		 
+		 
 		listenForInput();	
 	} catch (UnknownHostException e) {
 		e.printStackTrace();
@@ -43,16 +67,18 @@ public Client(){
 			e.printStackTrace();
 			}
 		 }
-		 
+		 String f = new String(": ");
 		 
 		 String input = console.nextLine();
+		 String conc1 = c.concat(f);
+		 String conc = conc1.concat(input);
 		 
 		 if(input.toLowerCase().equals("quit")) {
 			 break;
 			 
 		 }
-		cc.sendStringToServer(input);
-		 
+		//cc.sendStringToServer(input);
+		cc.sendStringToServer(conc);
 		}
 	 cc.close();
 	}
